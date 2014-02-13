1da4723090b236e47986d5d1d9af4765fffc991f
Genjo15 <anthony.chhim@gmail.com>
2014-02-13 18:05:21 +0100

    BACKUP Project

---
+++ Airport WCF Client/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Airport_WCF_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.radioButton3);
+            this.splitContainer.Panel1.Controls.Add(this.label);
+            this.splitContainer.Panel1.Controls.Add(this.textBox);
+            this.splitContainer.Panel1.Controls.Add(this.radioButton2);
+            this.splitContainer.Panel1.Controls.Add(this.radioButton);
+            this.splitContainer.Panel1.Controls.Add(this.button1);
+            this.splitContainer.Panel1MinSize = 1;
+            this.splitContainer.Panel2MinSize = 1;
+            this.splitContainer.Size = new System.Drawing.Size(834, 298);
+            this.splitContainer.SplitterDistance = 221;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Calibri", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(46, 135);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(90, 14);
+            this.label.TabIndex = 6;
+            this.label.Text = "Entrer ID du Vol :";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(46, 156);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(136, 20);
+            this.textBox.TabIndex = 5;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(23, 60);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(123, 19);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Récupérer Bagage";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton
+            // 
+            this.radioButton.AutoSize = true;
+            this.radioButton.Checked = true;
+            this.radioButton.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton.Location = new System.Drawing.Point(23, 35);
+            this.radioButton.Name = "radioButton";
+            this.radioButton.Size = new System.Drawing.Size(102, 19);
+            this.radioButton.TabIndex = 3;
+            this.radioButton.TabStop = true;
+            this.radioButton.Text = "Récupérer Vol";
+            this.radioButton.UseVisualStyleBackColor = true;
+            this.radioButton.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(46, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chercher!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(23, 85);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(175, 19);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Récupérer Bagages d\'un Vol";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 298);
+            this.Controls.Add(this.splitContainer);
+            this.MaximumSize = new System.Drawing.Size(850, 337);
+            this.MinimumSize = new System.Drawing.Size(850, 337);
+            this.Name = "Form1";
+            this.Text = "Aéroport de Paris - Le monde entier est notre invité";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Airport WCF Client/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Airport_WCF_Client
+{
+    public partial class Form1 : Form
+    {
+        DataGridView grid = new DataGridView();
+        //DataGridView grid2 = new DataGridView();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton.Checked)
+            {
+                Airport_WCF_Client.AirportService.Vol vol = new Airport_WCF_Client.AirportService.Vol();
+                AirportService.Service1Client client = new AirportService.Service1Client();
+                vol = client.GetVol(int.Parse(textBox.Text));
+
+                //richTextBox.Text = "ID Vol : " + vol.idVol + "\n" +
+                //                   "Compagnie : " + vol.company + "\n" +
+                //                   "Ligne n° : " + vol.lineNumber + "\n" +
+                //                   "Dernier Enr. : " + vol.lastTime + "\n" +
+                //                   "Parking : " + vol.parking + "\n" +
+                //                   "Status : " + vol.status + "\n";
+
+                List<Airport_WCF_Client.AirportService.Vol> list = new List<Airport_WCF_Client.AirportService.Vol>();
+                list.Add(vol);
+
+                grid.DataSource = list;
+                splitContainer.Panel2.Controls.Add(grid);
+                grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            }
+
+            else if (radioButton2.Checked)
+            {
+                Airport_WCF_Client.AirportService.Bagage bagage = new AirportService.Bagage();
+                AirportService.Service1Client client = new AirportService.Service1Client();
+                bagage = client.GetBagage(int.Parse(textBox.Text));
+
+                List<Airport_WCF_Client.AirportService.Bagage> list = new List<AirportService.Bagage>();
+                list.Add(bagage);
+
+                grid.DataSource = list;
+                splitContainer.Panel2.Controls.Add(grid);
+                grid.Dock = System.Windows.Forms.DockStyle.Fill;
+
+
+            }
+
+            else if (radioButton3.Checked)
+            {
+                Airport_WCF_Client.AirportService.Bagage[] bagagesList;
+                AirportService.Service1Client client = new AirportService.Service1Client();
+                bagagesList = client.GetBagagesFromVol(int.Parse(textBox.Text));
+
+
+                grid.DataSource = bagagesList;
+                splitContainer.Panel2.Controls.Add(grid);
+                grid.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label.Text = "Entrer ID du Bagage :";
+        }
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            label.Text = "Entrer ID du Vol :";
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label.Text = "Entrer ID du Vol :";
+        }
+    }
+}
